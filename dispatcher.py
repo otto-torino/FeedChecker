4e69fb78d717ece76fcbd60478e545efd4ba43f6
abidibo <abidibo@gmail.com>
2018-10-30 16:43:52 +0100

    rm unused signal

--- dispatcher.py
+++ dispatcher.py
@@ -7,5 +7,3 @@ from singleton import Singleton
 class Dispatcher(QtCore.QObject):
     error = QtCore.pyqtSignal(str, name='commandError')
     command_complete = QtCore.pyqtSignal(int, name='commandComplete')
-    rsnapshot_firstset = QtCore.pyqtSignal(
-        name='rsnapshotConfPathFirstSet')
